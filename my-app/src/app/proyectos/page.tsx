9a8980732e2ec24af1b81470edbc00545e0feee9
Candela Buttigliero <cbuttigliero@hotmail.com>
2024-05-23 10:56:55 +0200

    building collapsable component and projects pages

---
+++ my-app/src/app/proyectos/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Collapsable from '../../components/Collapsable/Collapsable';
+import React, { useEffect, useRef } from 'react';
+
+const Proyectos = () => {
+  return (
+    <div>
+      <Collapsable open title='Libros'>
+        Consectetur adipiscing elit pellentesque habitant morbi tristique.
+        Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
+        pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
+        et netus et. Elementum integer enim neque volutpat. Faucibus in ornare
+        quam viverra orci sagittis. Amet volutpat consequat mauris nunc congue
+        nisi vitae suscipit. Dui accumsan sit amet nulla. Proin sagittis nisl
+        rhoncus mattis. Enim nulla aliquet porttitor lacus. Arcu odio ut sem
+        nulla pharetra diam sit amet. Gravida rutrum quisque non tellus orci ac
+        auctor augue
+      </Collapsable>
+      <Collapsable open title='Guion'>
+        Consectetur adipiscing elit pellentesque habitant morbi tristique.
+        Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
+        pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
+        et netus et. Elementum integer enim neque volutpat. Faucibus in ornare
+        quam viverra orci sagittis. Amet volutpat consequat mauris nunc congue
+        nisi vitae suscipit. Dui accumsan sit amet nulla. Proin sagittis nisl
+        rhoncus mattis. Enim nulla aliquet porttitor lacus. Arcu odio ut sem
+        nulla pharetra diam sit amet. Gravida rutrum quisque non tellus orci ac
+        auctor augue
+      </Collapsable>
+      <Collapsable open title='Proyectos en desarrollo'>
+        Consectetur adipiscing elit pellentesque habitant morbi tristique.
+        Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
+        pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
+        et netus et. Elementum integer enim neque volutpat. Faucibus in ornare
+        quam viverra orci sagittis. Amet volutpat consequat mauris nunc congue
+        nisi vitae suscipit. Dui accumsan sit amet nulla. Proin sagittis nisl
+        rhoncus mattis. Enim nulla aliquet porttitor lacus. Arcu odio ut sem
+        nulla pharetra diam sit amet. Gravida rutrum quisque non tellus orci ac
+        auctor augue
+      </Collapsable>
+    </div>
+  );
+};
+export default Proyectos;
---
+++ my-app/src/components/Collapsable/Collapsable.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa6';
+
+interface IProps {
+  open?: boolean;
+  title: string;
+}
+
+const Collapsible: React.FC<IProps> = ({ open, children, title }) => {
+  const [isOpen, setIsOpen] = useState();
+  const [height, setHeight] = useState(0);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) setHeight(ref.current?.getBoundingClientRect().height);
+    else setHeight(0);
+  }, [isOpen]);
+
+  const handleFilterOpening = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (!height || !isOpen || !ref.current) return undefined;
+    const resizeObserver = new ResizeObserver((el) => {
+      setHeight(el[0].contentRect.height);
+    });
+    resizeObserver.observe(ref.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [height, isOpen]);
+
+  return (
+    <>
+      <div className='card' onClick={handleFilterOpening}>
+        <div>
+          <div className='collapsable-wrapper'>
+            <h6 className='font-weight-bold'>{title}</h6>
+            <button type='button' className='btn' onClick={handleFilterOpening}>
+              {!isOpen ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+        <div className='border-bottom my-collapse' style={{ height }}>
+          <div ref={ref}>
+            <div className='p-3'>{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Collapsible;
