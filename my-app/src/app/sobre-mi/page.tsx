3368bb52a7e1ce132ae46f8ff3e944790673ca4c
Candela Buttigliero <candela.buttigiero@ES282M-YY6X62R.local>
2025-07-24 16:22:12 +0200

    fix: text

--- my-app/src/app/sobre-mi/page.tsx
+++ my-app/src/app/sobre-mi/page.tsx
@@ -22,8 +22,7 @@ const SobreMi = () => {
               alt='Picture of the author'
             />
             <div className='text'>
-              <p>
-                Guionista y analista de guion especializada en desarrollo de cine y series.</p>
+                <p>Guionista y analista de guion especializada en desarrollo de cine y series.</p>
                 <p><b>Beca Netflix – Creative Equity 2022</b> para el Máster de Guion en la <b>Escola de Cinema de Catalunya (ESCAC)</b>, ganadora del concurso <b>Audi Future Stories 2023</b> y del concurso de <b>Series Cortas del INCAA 2024</b>.</p>
                 <p>Trabajo con una voz autoral sólida y sensibilidad narrativa, integrando perspectiva DEI desde la fase de escritura. Mi experiencia incluye mesas de guionistas, desarrollo de pitch decks, script doctoring y análisis narrativo para productoras en España y Latinoamérica. He trabajado con ONZA, Magma Cine, Indómita TV y Arena Collective y con operadoras como Warner, TNT y HBO Max. Fundadora de Diversidad en el Cine y analista voluntaria para el Observatorio de Diversidad Audiovisual de España.</p>
             </div>
